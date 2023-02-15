71c5fff7f9a1464d4338d87109bc3a66c5eee441
jhonasps <jhonas@purespectrum.com>
2023-02-15 09:14:21 -0300

    ADD cli project

---
+++ cli/weekplanner/app.py
@@ -0,0 +1,75 @@
+from .src.activity import Activity
+from .src.repository import activities, list_day_activities
+from .ui import select_time, edit_activity, assign_activity, info, confirm, select_activity
+
+def init():
+    day, selected_act = list_day_activities()
+    show_day(day, selected_act)
+
+def show_day(day: list[Activity], start_row):
+    buffer: list[str] = []
+    buffer.append('NEW Activity')
+    buffer.append('CHANGE Activity')
+    buffer.append('DELETE Activity')
+    buffer += [str(act) for act in day]
+    buffer.append('QUIT exit')
+    selected = select_time(buffer, start_row + 4)
+    if selected:
+        if 'exit' in selected:
+            return
+        index = buffer.index(selected)
+        change_options = ['NEW', 'CHANGE', 'DELETE']
+        if 'Activity' in selected:
+            option = selected.split(' ')[0]
+            if 'DELETE' in selected:
+                delete_activity()
+            elif option in change_options:
+                change_activity(option)
+            else:
+                print('INVALID OPTION 🤔')
+        else:
+            act = day[index - 3]
+            activity_assignment(act)
+            # print('i:', index, 's:', selected, 'a:', act)
+        init()
+
+def change_activity(action):
+    if action == 'NEW':
+        print('yes new')
+        act = edit_activity()
+        if act:
+            activities.append(act)
+    elif not activities:
+        info('No activities registered')
+    else:
+        act = activity_selection('CHANGE Activity')
+        if act:
+            edit_activity(act)
+
+def delete_activity():
+    if not activities:
+        info('No activities registered')
+    else:
+        act = activity_selection('DELETE Activity')
+        response = confirm(f'Are you sure you want to delete the activity {act.name}?')
+        if response:
+            activities.remove(act)
+
+def activity_selection(title):
+    buffer = [str(act) for act in activities]
+    selected = select_activity(title)# put buffer on this?
+    index = buffer.index(selected)
+    return activities[index]
+
+def activity_assignment(act: Activity):
+    time = act.time()
+    # import pdb;pdb.set_trace()
+    if act.is_free() and not activities:
+        act = edit_activity(act, True)
+    # selected_act = activity_selection(f'SELECT one activity to {time}')
+    # selected_act.start = act.start
+    # selected_act.end = act.end
+    assign_activity(act)
+
+if __name__ == 'semanapp.app':
+    init()
---
+++ cli/weekplanner/ui.py
@@ -0,0 +1,134 @@
+from subprocess import Popen, PIPE
+from pyfzf.pyfzf import FzfPrompt
+from datetime import datetime
+from .src.activity import Activity, week_time, week_day
+from .src.repository import activities
+
+fzf = FzfPrompt()
+
+def single_result(selected):
+    if not selected:
+        return None
+    return selected[0]
+
+def select_time(lines, row):
+    time = week_time()
+    selected = fzf.prompt(
+        lines,
+        f'--reverse --sync --header "⏰ {time}" --bind start:pos:{row}'
+    )
+    return single_result(selected)
+
+def select_activity(title):
+    selected = fzf.prompt(
+        activities,
+        f'--reverse --sync --header "🏋 {title}"'
+    )
+    return single_result(selected)
+
+def edit_activity(act: Activity=None, warn: bool=False):
+    if warn:
+        info("There's no activities registered, you're being redirected to create a new activity")
+    changed_act = act.clone() if act else Activity()
+
+    cmd_header('New Activity:')
+
+    if not changed_act.is_free():
+        option = confirm(f'Do you want to change the name from {act.name}')
+        if option:
+            changed_act.name = input('name: ')
+        option = confirm(f'Do you want to change the color from {act.name}')
+        if option:
+            changed_act.color = input('color: ')
+    else:
+        changed_act.name = input('name: ')
+        changed_act.color = input('color: ')
+    changed_act.notification = confirm('Do you want to notify when this activity is about to start?')
+    changed_act.alarm = confirm('Do you want to set a alarm sound when this activity is about to start?')
+    if changed_act.alarm:
+        changed_act.alarm = select('Select alarm sound:', ['old', 'ring', 'android'])
+
+    # cmd_header('The new activity:')
+    # print(changed_act.detail())
+    # print()
+
+    save = confirm('Do you want to save?', preview=changed_act.detail())
+    if save:
+        if act and not act.is_free():
+            act.update(changed_act)
+        return changed_act
+    else:
+        return None
+
+def select_datetime(title: str, date: datetime):
+    # week_days = list(map(lambda i: week_day(i), range(7)))
+    # day = select(f'{title} - day of the week:', week_days)
+    # day = week_days.index(day)
+    # hour = select(f'{title} - hour:', range(24))
+    hour = str(date.hour).rjust(2, '0')
+    fifteen_minutes = lambda i: str(i * 15).rjust(2, '0')
+    minutes=list(map(
+        lambda i: f'{hour}:{fifteen_minutes(i)}',
+        range(4)
+    ))
+    minute = select(f'{title} - minute:', minutes)
+    hour = int(hour)
+    minute = int(minute.split(':')[1])
+    return date.replace(hour=hour,minute=minute, second=0, microsecond=0)
+
+def assign_activity(act: Activity):
+    changed_act = act.clone()
+    changed_act.start = select_datetime('START DAY', act.start)
+    changed_act.end = select_datetime('END DAY', act.end)
+    time = changed_act.time()
+    
+    save = confirm(f'Do you want to assign {act.name} to {time}?',
+                   preview=changed_act.detail_full())
+    # TODO: verify time conflicts
+    # already_on_activities = list(filter(
+        # lambda a: a.name == act.name and a.start and a.end,
+        # activities
+    # ))
+    if save:
+        # if already_on_activities:
+            # activities.append(changed_act)
+        # else:
+            # act.update(changed_act)
+        activities.append(changed_act)
+    return changed_act
+
+def select(header, options):
+    response = fzf.prompt(options, f'--header "{header}"')
+    return single_result(response)
+
+def confirm(question: str, as_bool: bool = True, preview=None):
+    prev = ''
+    if preview:
+        prev = f'--preview "printf \'{preview}\'"'
+    response = fzf.prompt(['Yes', 'No'], f'--header "🤔 {question}" {prev}')
+    response = single_result(response)
+    if not response and as_bool:
+        return False
+    elif response and as_bool:
+        return response == 'Yes'
+    return response
+
+def info(say):
+    return fzf.prompt(['Ok'], f'--header "ℹ {say}"')
+
+def cmd_header(title):
+    print('--------------------------------------')
+    print(title)
+    print('--------------------------------------')
+    print()
+
+# process = Popen(command, stdout=PIPE, stderr=PIPE)
+# stdout, stderr = process.communicate()
+# print(stdout, stderr)
+# Popen(f"bash -c 'fzf --preselect=\\'{selected}\\' <<< \"{lines}\"'", shell=True)
+    # bash -c "fzf --preselect='{selected}' <<< '{lines}'"
+# with open('./temp', 'w') as f:
+    # f.write(lines)
+# cmd = f"cat ./temp | fzf --sync --bind start:pos:{row}"
+# print(cmd)
+# Popen(cmd, shell=True)
